b615de172ebb7427f2341d665ad9561803093f3b
Jack Hybl <69874924+Jack-Hybl@users.noreply.github.com>
2020-10-04 13:58:33 -0500

    adding in all forms

---
+++ forms/while/while.js
@@ -0,0 +1,9 @@
+let user_entry = Number(prompt("How many times do you want the cycle to run?"))
+
+let i = 0 
+
+while (i<= user_entry) {
+  console.log("This is loop iteration "+ i) 
+  i++
+  }
+console.log("This loop is now done")
